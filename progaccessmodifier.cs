a8fdd26cee1eacabc9101dae2d8f9126545037db
KonulHuseynli <konulrh@code.edu.az>
2022-05-31 17:10:29 +0400

    addedoldtask

---
+++ progaccessmodifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (var item in student1.Grades)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
---
+++ studentaccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp66
+{
+    class Student
+    {
+        private int[] _grades;
+        public int[] Grades
+        {
+            get
+            {
+                return _grades;
+            }
+            set
+            {
+                int count = 0;
+                foreach (var item in value)
+                {
+                    if (item % 2 == 0)
+                        count++;
+                }
+                int [] newArr = new int[count];
+                int newArrIndex = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        newArr[newArrIndex] = value[i];
+                        newArrIndex++;
+                    }
+                }
+                _grades = newArr;
+            }
+        }
+    }
+}
